cf418bf68b810aacc93ea3ecb992df59bcce7af1
booringreader <157519312+booringreader@users.noreply.github.com>
2024-04-02 20:41:37 +0530

    train model on 2 classes fo data (cat & wolf) for 20epochs

--- imgdet.py
+++ imgdet.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 import os
 import cv2  
 import imghdr #for checking file extensions
@@ -10,7 +12,7 @@ gpus= tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
-data_dir = '~/Developer/personal/halide/data'
+data_dir = 'data'
 
 image_exts = ['jpg', 'jpeg']
 
@@ -28,7 +30,52 @@ for image_class in os.listdir(data_dir):
             print('invalid extension'.format(image_path))
 
 # load data
-tf.Data.Dataset
-data = tf.keras.utils.image_dataset_from_directory('data')
+tf.data.Dataset
+data = tf.keras.utils.image_dataset_from_directory('data') # building data pipeline
 
-data_iterator = data.as_numpy_iterator()
+data_iterator = data.as_numpy_iterator() # allows access to data pipeline
+batch = data_iterator.next() # accesses the data pipeline
+
+fig, ax = plt.subplots(ncols=4, figsize=(20,20))
+for idx, img in enumerate(batch[0][:4]):
+    ax[idx].imshow(img)
+    ax[idx].title.set_text(batch[1][idx])
+
+# pre-process the data
+
+#scale data
+data = data.map(lambda x,y: (x/255, y))
+
+train_size = int(len(data)* .7)
+val_size = int(len(data)* .2) + 1  
+test_size = int(len(data)* .1) + 1
+
+train = data.take(train_size)
+val = data.skip(train_size).take(val_size)
+test = data.skip(train_size+val_size).take(test_size)
+
+
+model = Sequential()
+model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256, 256, 3)))
+model.add(MaxPooling2D())
+
+model.add(Conv2D(16, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+
+model.add(Conv2D(16, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+
+model.add(Flatten())
+
+model.add(Dense(256, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+
+model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
+model.summary
+
+
+logdir = 'logs'
+
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
